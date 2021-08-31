0f697d32d1570ac9c544d55b2c2fd9c75fdff0ab
ratinac-nikola <ratinac.nikola@gmail.com>
2021-08-31 18:57:31 +0200

    change return type from 'bytes' to 'str'

--- ecs-ml-training-service/service/serialization.py
+++ ecs-ml-training-service/service/serialization.py
@@ -4,7 +4,7 @@ from typing import Any
 
 def serialize(model: Any) -> str:
     pickled_model = pickle.dumps(model)
-    return base64.b64encode(pickled_model)
+    return str(base64.b64encode(pickled_model))
 
 def deserialize(bytestring: str) -> Any:
     pickled_model = base64.b64decode(bytestring)
