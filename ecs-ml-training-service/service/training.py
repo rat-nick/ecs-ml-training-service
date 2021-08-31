81a1cab9c69645bfcf857b5d288f0e82abe10759
ratinac-nikola <ratinac.nikola@gmail.com>
2021-08-31 18:51:34 +0200

    add model training

---
+++ ecs-ml-training-service/service/training.py
@@ -0,0 +1,15 @@
+from pandas.core.frame import DataFrame
+from sklearn.tree import DecisionTreeRegressor
+from typing import List
+
+def train(data: DataFrame, response_vars: List[str]) -> DecisionTreeRegressor:
+    # split response and predictor vars
+    y = data[response_vars]
+    predictor_vars = data.columns.difference(response_vars)
+    X = data[predictor_vars]
+    # create and fit the model to the data
+    model = DecisionTreeRegressor()
+    model.fit(X, y)
+    
+    return model
+    
