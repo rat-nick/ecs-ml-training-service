f07d439f88b6d81f30cc7592c7895cc1684b431e
ratinac-nikola <ratinac.nikola@gmail.com>
2021-08-31 18:54:18 +0200

    add typing to serialization

--- ecs-ml-training-service/service/serialization.py
+++ ecs-ml-training-service/service/serialization.py
@@ -1,11 +1,12 @@
 import base64
 import pickle
+from typing import Any
 
-def serialize(model):
+def serialize(model: Any) -> str:
     pickled_model = pickle.dumps(model)
     return base64.b64encode(pickled_model)
 
-def deserialize(bytestring: str):
+def deserialize(bytestring: str) -> Any:
     pickled_model = base64.b64decode(bytestring)
     return pickle.loads(pickled_model)
 
