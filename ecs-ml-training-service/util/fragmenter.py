772f2f7111bbdb29d2cb18c68d3fa4d3fac9c0ca
ratinac-nikola <ratinac.nikola@gmail.com>
2021-08-31 13:25:48 +0200

    add fragment function

--- ecs-ml-training-service/util/fragmenter.py
+++ ecs-ml-training-service/util/fragmenter.py
@@ -1,4 +1,17 @@
 import pandas as pd
+import random
 from optparse import OptionParser
 
+# TODO: function to get a random subset of rows from a .csv as dataframe
+def fragment(fpath, fragment_size):
+    content = pd.read_csv(fpath, header=0)
+    n = len(content)
+    s = fragment_size
+    skip = sorted(random.sample(range(1, n+1), n-s))
+    df = pd.read_csv(fpath, skiprows=skip)
+    return df
 
+#TODO: function to create n random fragments of a file
+
+#TODO: create module entry
+    
