b8508dd841d1896ee80772ff258322b964e20d72
ratinac-nikola <ratinac.nikola@gmail.com>
2021-09-09 14:04:54 +0200

    feat(train): model from decision tree to MLP

--- server/services/training.py
+++ server/services/training.py
@@ -1,15 +1,15 @@
 from pandas.core.frame import DataFrame
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPRegressor
 from typing import List
 
 
-def train(data: DataFrame, response_vars: List[str]) -> DecisionTreeRegressor:
+def train(data: DataFrame, response_vars: List[str]) -> MLPRegressor:
     # split response and predictor vars
     y = data[response_vars]
     predictor_vars = data.columns.difference(response_vars)
     X = data[predictor_vars]
     # create and fit the model to the data
-    model = DecisionTreeRegressor()
+    model = MLPRegressor()
     model.fit(X, y)
 
     return model
